5e65162bff6a09d2a660595526c0a1b0a1c5245b
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-31 11:32:50 -0300

    Modulo Teste Unitários concluido

--- CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
+++ CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc9c86fa23cd9ac170a3a001a7a7f147f570cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d551d0dfdf8ef5d5ee9c18364b9f4214eccc442")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
