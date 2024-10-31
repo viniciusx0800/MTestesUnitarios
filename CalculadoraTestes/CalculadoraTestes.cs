944c57887e307754a53b869dcec2410404c30a0c
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-31 10:06:54 -0300

    feat: fazer testes no projeto

--- CalculadoraTestes/CalculadoraTestes.cs
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -25,4 +25,18 @@ public class CalculadoraTestes
         // Assert
         Assert.Equal(15, resultado);
     }
+
+    [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
+
+        // Act
+        int resultado = _calc.Somar(num1, num2); 
+
+        // Assert
+        Assert.Equal(20, resultado);
+    }
 }
